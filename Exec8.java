c1951788cfda0b431c29e3017064816325c83931
Arttanjeiro <102182959+Arttanjeiro@users.noreply.github.com>
2022-04-01 11:10:56 -0300

    Novo arquivo

--- Exec8.java
+++ Exec8.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 /**
- * Exec8
+ O código abaixo deve ser reescrito  criando dois métodos além do método main. A parte do código em negrito deve estar dentro de um método de nome validar e parte em itálico deve estar em outro método de nome computarPrimos. Os dois métodos devem ser chamadas a partir do método main. Ao executar o programa ele deve ter o mesmo comportamento visual do programa Prog5.java (contido na Aula8).
+Os métodos criados podem ser com retorno ou sem retorno, com passagem de parâmetros ou sem passagem de parâmetros podendo ser método de classe ou de objeto, você programador deve decidir isso.
  */
 public class Exec8 {
     static  int num , cont =0;
